856b29718f8b8ccf0d0c5ef9f124055f037342b1
mot0laura <mot0laura@gmail.com>
2018-12-29 14:45:33 +1100

    [feat] customised layout with markdown slot

---
+++ docs/.vuepress/components/SpecialLayout.vue
@@ -0,0 +1,38 @@
+
+<template>
+  <div class="container">
+    <header>
+      <Content slot-key="header"/>
+      <h3>{{$frontmatter.title}}</h3>
+    </header>
+    <main>
+      <Content/>
+    </main>
+    <footer>
+      <Content slot-key="footer"/>
+    </footer>
+  </div>
+</template>
+
+<script>
+
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.container{
+  font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 16px;
+  color: #2c3e50;
+  .header{
+    color: pink;
+  }
+  .footer{
+    color: palegreen;
+  }
+}
+
+
+</style>
---
+++ docs/customised.md
@@ -0,0 +1,14 @@
+---
+layout: SpecialLayout
+title: Hey customised component
+---
+::: slot header
+# 323
+:::
+
+- A Paragraph
+- Another Paragraph
+
+::: slot footer
+Here's some contact info
+:::
